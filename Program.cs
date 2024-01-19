c35d3f1bed6f1ea66e6632c81e032fe2e949437a
geheron <geherona7@gmail.com>
2024-01-19 14:11:33 +0530

    Add Initial Files

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            var queryResult = from num in arr
+                              where (num % 2 == 0)
+                              select num;
+
+            Console.WriteLine("Query syntax result:");
+            foreach(var item in queryResult)
+            {
+                Console.WriteLine(item);
+            }
+
+            var methodResult1 = arr.Where(num => num % 2 == 0);
+            var methodResult = methodResult1.Select(num => num);
+
+            Console.WriteLine("Method syntax result:");
+            foreach (var item in methodResult)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
